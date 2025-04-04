f82c371e99b1b9f1c682e97ea03e33e8ddc38cea
PlettserVladimir <129154911+PlettserVladimir@users.noreply.github.com>
2025-04-04 12:19:07 +0700

    Test commit

--- Battle(Camunda)/src/main/java/com/example/delegate/EvaluateBattle.java
+++ Battle(Camunda)/src/main/java/com/example/delegate/EvaluateBattle.java
@@ -2,10 +2,14 @@ package com.example.delegate;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EvaluateBattle implements JavaDelegate {
+    private static final Logger LOG = LoggerFactory.getLogger(EvaluateBattle.class);
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         int warriors = (int) delegateExecution.getVariable("warriors");
@@ -18,6 +22,7 @@ public class EvaluateBattle implements JavaDelegate {
             battleStatus = "Victory";
             delegateExecution.setVariable("isWin",isWin);
             delegateExecution.setVariable("battleStatus",battleStatus);
+            LOG.info("Battle status {}",battleStatus);
         }else {
             battleStatus = "Defeat";
             delegateExecution.setVariable("isWin",isWin);
